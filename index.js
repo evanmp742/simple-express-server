74652ca0879a637a99937d70051f0f484de8c697
evanmp742 <evanmatthewpoole@gmail.com>
2021-01-09 13:04:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (_, res) => {
-    res.send('yeah yeah okkkkk okokokok')
+    res.send('running from coder school host!')
 })
 
 app.listen(5050, () => {
